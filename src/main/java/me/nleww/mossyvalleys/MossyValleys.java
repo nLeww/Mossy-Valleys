f1d64f412d03b1d2efee24bbecf50e6550fe78fb
Ahlistar <nicolaslew005@gmail.com>
2024-11-04 17:27:37 -0300

    Initial commit

---
+++ src/main/java/me/nleww/mossyvalleys/MossyValleys.java
@@ -0,0 +1,19 @@
+package me.nleww.mossyvalleys;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MossyValleys implements ModInitializer {
+	public static final String MOD_ID = "mossyvalleys";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/me/nleww/mossyvalleys/MossyValleysClient.java
@@ -0,0 +1,11 @@
+package me.nleww.mossyvalleys;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MossyValleysClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
